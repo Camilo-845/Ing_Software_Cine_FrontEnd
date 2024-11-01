a0090c4fef4f60ee5fef04a0840100088d929e18
CamiloSar <camilo.sarmiento.amado@gmail.com>
2024-11-01 15:29:15 -0500

    app routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { OrdenesComponent } from './componentes/ordenes/ordenes.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { ErrorcitoComponent } from './componentes/errorcito/errorcito.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: InicioComponent },
+    { path: 'clients', component: ClientesComponent },
+    { path: 'orders', component: OrdenesComponent },
+    { path: 'products', component: ProductosComponent },
+    { path: 'dashboard', component: TableroComponent },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: ErrorcitoComponent }
+];
