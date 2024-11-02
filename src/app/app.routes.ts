6c763956e50be76ecdc3e4404bd8e32d879edfe6
Edmh1 <edavidmanotas@gmail.com>
2024-11-01 22:03:55 -0500

    creacion de componentes, ingreso, ordenes, errorcito,productos, registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,18 @@ import { ComidasComponent } from './componentes/comidas/comidas.component';
 import { ReservacionesComponent } from './componentes/reservaciones/reservaciones.component';
 import { CinesComponent } from './componentes/cines/cines.component';
 import { CartelerasComponent } from './componentes/carteleras/carteleras.component';
+import { OrdenesComponent } from './componentes/ordenes/ordenes.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { IngresoComponent } from './componentes/ingreso/ingreso.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 export const routes: Routes = [
     { path: 'home', component: InicioComponent },
-    { path: 'dashboard', 
-      component: TableroComponent, 
+    { path: 'orders', component: OrdenesComponent},
+    { path: 'products', component: ProductosComponent},
+    { path: 'login', component: IngresoComponent},
+    { path: 'sign-up', component: RegistroComponent},
+    { path: 'dashboard', component: TableroComponent, 
       children:[
         {path: "clientes", component: ClientesComponent},
         {path: "comidas", component: ComidasComponent},
