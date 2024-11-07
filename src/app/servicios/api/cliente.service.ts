49eb11cd81a0a27c4add505f973735fcb867fe54
Dan1el71 <ddrmhk1@gmail.com>
2024-11-07 13:25:47 -0500

    Implement reactive form for AgregarComponent and integrate client creation functionality

--- src/app/servicios/api/cliente.service.ts
+++ src/app/servicios/api/cliente.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import {
   API_CLIENTE,
+  API_CLIENTE_ADD,
   API_CLIENTE_LIST,
   API_CLIENTE_PAGINATION,
 } from '../../utils/URIs';
@@ -26,5 +27,9 @@ export class ClienteService {
 
   public getById(id: number): Observable<Cliente[]> {
     return this.httpClient.get<Cliente[]>(API_CLIENTE + `/${id}`);
+  }
+
+  public createClient(cliente: Cliente): Observable<any> {
+    return this.httpClient.post<Cliente>(API_CLIENTE_ADD, cliente);
   }
 }
