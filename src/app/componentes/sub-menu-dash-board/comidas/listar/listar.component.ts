2dfd59c5d6b8e9332277c4bbbc2bcc09b9dc8de6
CamiloSar <camilo.sarmiento.amado@gmail.com>
2024-11-08 11:55:57 -0500

    conexion con backend componente lista comidas
    Se agrago la conexion a los endpoints del backend y una paginacion

--- src/app/componentes/sub-menu-dash-board/comidas/listar/listar.component.ts
+++ src/app/componentes/sub-menu-dash-board/comidas/listar/listar.component.ts
@@ -1,23 +1,10 @@
-import { Component, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectorRef, OnInit, inject } from '@angular/core';
 import { MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-export interface Comida {
-  id: number;
-  nombre: string;
-  precio: number;
-}
-
-const DATA: Comida[] = [
-  { id: 1, nombre: "hamburguesa clásica", precio: 120 },
-  { id: 2, nombre: "hamburguesa con queso", precio: 130 },
-  { id: 3, nombre: "hamburguesa doble", precio: 150 },
-  { id: 4, nombre: "hamburguesa de pollo", precio: 110 },
-  { id: 5, nombre: "hamburguesa vegana", precio: 140 },
-  { id: 6, nombre: "hamburguesa con bacon", precio: 160 },
-  { id: 7, nombre: "hamburguesa BBQ", precio: 135 }
-];
+import { Comida } from '../../../../interfaces/comidas';
+import { ComidaService } from '../../../../servicios/api/comidas.service';
 
 @Component({
   selector: 'app-listar',
@@ -26,12 +13,40 @@ const DATA: Comida[] = [
   templateUrl: './listar.component.html',
   styleUrls: ['./listar.component.css']
 })
-export class ListarComponent{
-  columnas: string[] = ['id', 'nombre', 'precio'];
-  dataSource = new MatTableDataSource<Comida>(DATA); // Declaramos dataSource sin asignar datos
+export class ListarComponent implements OnInit{
+  private comidaService = inject(ComidaService);
+  private page:number = 1;
+  private limit:number = 10;
+  private DATA: Comida[] = [];
+  ngOnInit(): void {
+    this.getList();
+  }
+  columnas: string[] = ['idComida', 'nombreComida', 'precioComida'];
+  dataSource = new MatTableDataSource<Comida>(this.DATA);
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+  nextPage(){
+    this.page += 1;
+    this.getList();
+  }
+  prevPage(){
+    if(this.page > 1){
+      this.page -= 1;
+      this.getList();
+    }
+  }
+  private getList(): void {
+    this.comidaService.getAll(this.page, this.limit).subscribe((res) => {
+      this.DATA = res.map((el) => ({
+        nombreComida: el.nombreComida,
+        idComida: el.idComida,
+        precioComida: el.precioComida
+      }));
+      this.dataSource.data = this.DATA;
+    });
+  }
+  
 }
