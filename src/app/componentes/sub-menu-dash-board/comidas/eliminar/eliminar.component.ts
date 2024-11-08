e1d02f52d37d33a90c4670bf0bed1aa0ad72f116
CamiloSar <camilo.sarmiento.amado@gmail.com>
2024-11-08 11:59:11 -0500

    conexion backend componente eliminar comida

--- src/app/componentes/sub-menu-dash-board/comidas/eliminar/eliminar.component.ts
+++ src/app/componentes/sub-menu-dash-board/comidas/eliminar/eliminar.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { ComidaService } from '../../../../servicios/api/comidas.service';
+import { Comida } from '../../../../interfaces/comidas';
 
 @Component({
   selector: 'app-eliminar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './eliminar.component.html',
-  styleUrl: './eliminar.component.css'
+  styleUrl: './eliminar.component.css',
 })
 export class EliminarComponent {
+  private comidaService = inject(ComidaService);
+  private formBuilder = inject(FormBuilder);
 
+  createForm = this.formBuilder.group({
+    id: 0,
+  });
+  handleSubmit(): void {
+    const { id } = this.createForm.value as { id: number };
+    const comida: Comida = {
+      idComida: id,
+      nombreComida :"",
+      precioComida: 0,
+    };
+    this.comidaService.deleteComida(id).subscribe();
+    this.createForm.reset();
+  }
 }
