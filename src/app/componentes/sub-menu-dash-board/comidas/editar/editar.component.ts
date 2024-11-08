581eb26c2c4e58972ad0640968fde7cd8879b538
CamiloSar <camilo.sarmiento.amado@gmail.com>
2024-11-08 11:58:40 -0500

    conexion backend componente elimmiar comida

--- src/app/componentes/sub-menu-dash-board/comidas/editar/editar.component.ts
+++ src/app/componentes/sub-menu-dash-board/comidas/editar/editar.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import { ComidaService } from '../../../../servicios/api/comidas.service';
+import { Comida } from '../../../../interfaces/comidas';
+
 
 @Component({
   selector: 'app-editar',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './editar.component.html',
   styleUrl: './editar.component.css'
 })
 export class EditarComponent {
+  private comidaService = inject(ComidaService);
+  private formBuilder = inject(FormBuilder);
 
+  createForm = this.formBuilder.group({
+    id:0,
+    nombre: "",
+    precio : 0,
+  })
+
+  handleSubmit(): void {
+    const { id , nombre, precio } = this.createForm.value as { id:number, nombre: string; precio: number };
+    const comida: Comida = {
+      idComida: id,
+      nombreComida: nombre || "",
+      precioComida: precio || 0
+    };
+    this.comidaService.updateComida(comida).subscribe();
+    this.createForm.reset();
+  }
 }
