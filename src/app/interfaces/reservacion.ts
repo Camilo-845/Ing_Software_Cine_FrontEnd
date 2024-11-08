36d9a0b793ebb50dcc9f47123b30a692df518efb
SebastianCA19 <castroarrietasebastiandavid@gmail.com>
2024-11-07 23:48:14 -0500

    "Updated app routes, components, and services for reservaciones feature, including agregar, eliminar, listar, and buscar functionality."

---
+++ src/app/interfaces/reservacion.ts
@@ -0,0 +1,7 @@
+export interface Reservacion{
+    idReservacion?: number,
+    nombrePersona: string,
+    idSilla?: number,
+    idHorario?: number,
+    precio?: number
+}
--- src/app/utils/URIs.ts
+++ src/app/utils/URIs.ts
@@ -7,3 +7,10 @@ export const API_CLIENTE_PAGINATION = API_CLIENTE + '/pagination';
 export const API_CLIENTE_ADD = API_CLIENTE + '/add';
 export const API_CLIENTE_UPDATE = API_CLIENTE + '/update';
 export const API_CLIENTE_DELETE = API_CLIENTE + '/delete';
+
+//Reservaciones
+export const API_RESERVACIONES = API_URL + '/reservation';
+export const API_RESERVACIONES_LIST = API_RESERVACIONES + '/getall';
+export const API_RESERVACIONES_PAGINATION = API_RESERVACIONES + '/pagination';
+export const API_RESERVACIONES_DELETE = API_RESERVACIONES + '/delete';
+export const API_RESERVACIONES_ADD = API_RESERVACIONES + '/add';
