914b6f3dc740b1b96d6c65a622fe42015f809aff
Dan1el71 <ddrmhk1@gmail.com>
2024-11-10 11:18:05 -0500

    Enhance pagination component: add double-click edit functionality, prevent action on current page, and optimize page number handling

--- src/app/componentes/sub-menu-dash-board/clientes/pagination/pagination.component.ts
+++ src/app/componentes/sub-menu-dash-board/clientes/pagination/pagination.component.ts
@@ -18,6 +18,8 @@ export class PaginationComponent {
   }
 
   handlePageChange(action: number | 'prev' | 'next'): void {
+    if (this.currentPage === action) return;
+
     if (action === 'prev' && this.currentPage > 1) {
       this.handlePagination.emit('prev');
     } else if (action === 'next' && this.currentPage < this.totalPages) {
@@ -27,29 +29,38 @@ export class PaginationComponent {
     }
   }
 
+  handleEditPage(): void {
+    const page = prompt('Enter page number:');
+    const pageNumber = Number(page);
+
+    if (
+      !isNaN(pageNumber) &&
+      Number.isInteger(pageNumber) &&
+      pageNumber > 0 &&
+      pageNumber <= this.totalPages
+    )
+      this.handlePagination.emit(pageNumber);
+  }
+
   getPages(): Array<number> {
-    const pages: Array<number> = [];
+    const pages: Array<number> = [1];
 
-    pages.push(1);
-
-    if (this.currentPage > 2) {
-      pages.push(this.currentPage - 1);
+    if (this.currentPage == 1 && this.totalPages > 2) {
+      pages.push(2);
     }
 
     if (!pages.includes(this.currentPage)) {
       pages.push(this.currentPage);
     }
 
-    if (this.currentPage < this.totalPages - 1) {
-      pages.push(this.currentPage + 1);
+    if (this.currentPage == this.totalPages && this.totalPages > 2) {
+      pages.pop();
+      pages.push(this.totalPages - 1);
+      pages.push(this.totalPages);
     }
 
     if (this.totalPages > 1 && !pages.includes(this.totalPages)) {
       pages.push(this.totalPages);
-    }
-
-    if (pages.length > 4) {
-      pages.splice(1, pages.length - 4);
     }
 
     return pages;
