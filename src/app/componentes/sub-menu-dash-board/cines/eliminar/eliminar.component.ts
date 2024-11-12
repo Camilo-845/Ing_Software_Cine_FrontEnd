2f528d193ddd8e1a4c131b43b54fc2f8d98b8079
Mateo <mateo.angulo1804@gmail.com>
2024-11-11 22:05:25 -0500

    se vea datos sobre cine a eliminar antes de eliminarlo

--- src/app/componentes/sub-menu-dash-board/cines/eliminar/eliminar.component.ts
+++ src/app/componentes/sub-menu-dash-board/cines/eliminar/eliminar.component.ts
@@ -13,6 +13,7 @@ import { CommonModule } from '@angular/common';
 export class EliminarComponent {
   private cineService = inject(CineService);
   cineEliminado = false;
+  cineData: any;
   deleteForm = new FormGroup({
     idCine: new FormControl(''),
   });
@@ -22,6 +23,24 @@ export class EliminarComponent {
     this.cineService.deleteCine(Number(idCine!)).subscribe((res) => {
       console.log('Cine eliminado');
       this.cineEliminado = true;
+      this.cineData = null;
     });
+  }
+
+  onLoadCineData(): void {
+    const idCine = this.deleteForm.get('idCine')?.value;
+    if (idCine) {
+      this.cineService.getById(Number(idCine)).subscribe(
+        (cine) => {
+          // Si se encuentran los datos del cine, los almacena en cineData
+          this.cineData = cine;
+        },
+        (error) => {
+          console.error('Error al obtener los datos del cine:', error);
+        }
+      );
+    } else {
+      console.warn('Por favor ingrese un ID de cine válido');
+    }
   }
 }
